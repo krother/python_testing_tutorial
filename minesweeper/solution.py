21e0a646b64dcb3ad3abc73e28d8771fb6b69558
Kristian Rother <krother@academis.eu>
2023-11-08 12:14:12 +0100

    cleaner solution

--- minesweeper/solution.py
+++ minesweeper/solution.py
@@ -1,11 +1,12 @@
 
+from itertools import product
+
 def count_mines(field, location):
     """calculates the number of mines in a x/y position"""
     x, y = location
     s = ''
-    if x > 0 and y > 0:
-        s += field[x-1][y-1]
-    ...
+    for xd, yd in product([-1, 0, +1], [-1, 0, +1]):
+        xnew, ynew = x + xd, y + yd
+        if 0 <= xnew <= len(field[0]) and 0 <= y <= len(field):
+            s += field[y-1][x-1]
     return s.count('*')
-
-
